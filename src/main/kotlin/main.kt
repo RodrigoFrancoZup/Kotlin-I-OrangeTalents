690f7bb82d21b81d7be76861bac752da3fff177f
Rodrigo Franco <rodrigo.franco@zup.com.br>
2021-05-26 15:59:16 -0300

    Escrevendo código mais idiomático ao Kotlin

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,46 +4,69 @@ fun main() {
     //fluxoDeRepeticao()
     //entendendoDiferencaEntreCopiaEReferencia()
 
-    val contaRodrigo = Conta()
-    contaRodrigo.setTitular("Rodrigo")
-    contaRodrigo.setNumero(1000)
-    contaRodrigo.setSaldo(200.0)
+    //Usando Label no patâmetos do construtor!
+    val contaRodrigo = Conta(numero = 1000, titular = "Rodrigo")
 
-    val contaFran = Conta()
-    contaFran.setTitular("Fran")
-    contaFran.setNumero(1001)
-    contaFran.setSaldo(300.00)
+    contaRodrigo.deposita(2000.0)
 
-    println(contaRodrigo.getTitular())
-    println(contaFran.getTitular())
+    val contaFran = Conta("Franco", 1001)
+
+    contaFran.deposita(300.0)
+
+    println(contaRodrigo.titular)
+    println(contaRodrigo.numero)
+    println(contaFran.titular)
+    println(contaFran.numero)
 
 
-    println("Depositando na conta do ${contaRodrigo.getTitular()}")
+    println("Depositando na conta do ${contaRodrigo.titular}")
     contaRodrigo.deposita(50.0)
-    println("Saldo da conta Rodrigo é de: ${contaRodrigo.getSaldo()}")
+    println("Saldo da conta Rodrigo é de: ${contaRodrigo.saldo}")
 
-    println("Saque na conta do ${contaRodrigo.getTitular()}")
+    println("Saque na conta do ${contaRodrigo.titular}")
     contaRodrigo.saca(200.0)
-    println("Saldo da conta Rodrigo é de: ${contaRodrigo.getSaldo()}")
+    println("Saldo da conta Rodrigo é de: ${contaRodrigo.saldo}")
 
-    println("Transferencia da conta do ${contaRodrigo.getTitular()} para a conta da ${contaFran.getTitular()}")
+    println("Transferencia da conta do ${contaRodrigo.titular} para a conta da ${contaFran.titular}")
     if(contaRodrigo.transfere(50.00, contaFran)){
         println("Transferencia Ok!")
     }else{
         println("Falha na Transferencia!")
     }
-    println("Saldo da conta Fran é de: ${contaFran.getSaldo()}")
+    println("Saldo da conta Fran é de: ${contaFran.saldo}")
 
 }
 
 //Criando a classe Conta - Vamos melhorar no futuro
-class Conta{
-    private var titular = ""
-    private var numero = 0
-    private var saldo = 0.0
+class Conta(var titular: String, var numero: Int){
+     var saldo = 0.0
+         private set
+
+    /* --Construtor Secundário
+
+    constructor(titular: String, numero: Int){
+        this.titular = titular
+        this.numero = numero
+    }
+    */
+
+
+    /*
+    - Caso eu quisesse colocar uma regra de négocio, no set padrao da propertie saldo:
+    - valor é de fato o valor que eu estou passando para a propertie
+    - field é de fato a atribuição do valor para a propertie, é ele que injeta o valor!
+        var saldo = 0.0
+        set(valor){
+            if(valor > 1000){
+                field = valor
+            }
+        }
+     */
 
     fun deposita(valor: Double){
-        this.saldo += valor
+        if(valor > 0){
+            this.saldo += valor
+        }
     }
 
     fun saca(valor: Double){
@@ -62,7 +85,8 @@ class Conta{
         }
             return false
     }
-
+/*
+    -- Não usamos getter e setter em Kotlin
     fun getSaldo() : Double{
         return this.saldo
     }
@@ -86,6 +110,8 @@ class Conta{
     fun setNumero(numero: Int){
         this.numero = numero
     }
+
+ */
 }
 
 
@@ -195,50 +221,46 @@ fun entendendoDiferencaEntreCopiaEReferencia(){
     //É igual no java
     //contaRodrigo é uma referencia para o objeto do tipo Conta
 
-//    val contaRodrigo = Conta()
-//    contaRodrigo.titular = "Rodrigo"
-//    contaRodrigo.numero = 1000
-//    contaRodrigo.saldo = 200.0
-//
-//    val contaFran = Conta()
-//    contaFran.titular = "Fran"
-//    contaFran.numero = 1001
-//    contaFran.saldo = 300.0
-//
-//    println(contaRodrigo.titular)
-//    println(contaRodrigo.numero)
-//    println(contaRodrigo.saldo)
-//
-//    println(contaFran.titular)
-//    println(contaFran.numero)
-//    println(contaFran.saldo)
+    val contaRodrigo = Conta("Rodrigo", 1000)
+   // Nao funciona mais, pois o set de saldo agora é private contaRodrigo.saldo = 200.0
+
+    val contaFran = Conta("Fran", 1001)
+    // Nao funciona mais, pois o set de saldo agora é private contaFran.saldo = 300.0
+
+    println(contaRodrigo.titular)
+    println(contaRodrigo.numero)
+    println(contaRodrigo.saldo)
+
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
 
     // Inicio do: Entendo copia e referencia:
 
     // --Variaveis com tipo Primitivo:
 
-//    val numeroX = 10
-//    var numeroY = numeroX
-//    numeroY++
+    val numeroX = 10
+    var numeroY = numeroX
+    numeroY++
 
     //Apenas o numeroY foi alterado! Pois com número primitivo ocorre a Copia!
 
-//    println("numeroX $numeroX")
-//    println("numeroY $numeroY")
+    println("numeroX $numeroX")
+    println("numeroY $numeroY")
 
     // --Variaveis que são referencia de um objeto
 
-//    val contaJoao = Conta()
-//    contaJoao.titular = "João"
-//
-//    val contaMaria = contaJoao;
-//    contaMaria.titular = "Maria"
+    val contaJoao = Conta("Joao", 2000)
+
+
+    val contaMaria = contaJoao;
+    contaMaria.titular = "Maria"
 
     //Ambas as contas foram alteradas! Pois a variável que aponta para objeto é sempre uma referencia!
     //Apontam para o mesmo endereço de memoria!
 
-//    println("Titular de contaJoao é ${contaJoao.titular}")
-//    println("Titular de contaMaria é ${contaMaria.titular}")
+    println("Titular de contaJoao é ${contaJoao.titular}")
+    println("Titular de contaMaria é ${contaMaria.titular}")
 
     //Fim do: Entendendo copia e referencia.
 }
